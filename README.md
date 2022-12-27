12bf6dde0d03390516b2d51e6d397f9f19539d96
Nguyen Huy Hoang <hoangnh36@fsoft.com.vn>
2022-12-27 12:45:17 +0700

    Update comment of bikeshare_2.py and remove unused file in README

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 >**Note**: Please **fork** the current Udacity repository so that you will have a **remote** repository in **your** Github account. Clone the remote repository to your local machine. Later, as a part of the project "Post your Work on Github", you will push your proposed changes to the remote repository in your Github account.
 
 ### Date created
-12/27/2022.
+12/27/2022
 
 ### Project Title
 HoangNH36 - Udacity Course
@@ -11,9 +11,6 @@ Project to study the way to control Git version
 
 ### Files used
 bikeshare_2.py
-chicago.csv
-new_york_city.csv
-washington.csv
 
 ### Credits
 It's important to give proper credit. Add links to any repo that inspired you or blogposts you consulted.
--- bikeshare_2.py
+++ bikeshare_2.py
@@ -2,9 +2,11 @@ import time
 import pandas as pd
 import numpy as np
 
-CITY_DATA = { 'chicago': 'chicago.csv',
-              'new york city': 'new_york_city.csv',
-              'washington': 'washington.csv' }
+CITY_DATA = { 
+    'chicago': 'chicago.csv',
+    'new york city': 'new_york_city.csv',
+    'washington': 'washington.csv' 
+    }
 
 def get_filters():
     """
@@ -16,7 +18,7 @@ def get_filters():
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('Hello! Let\'s explore some US bikeshare data!')
-    # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
+    # get user input for city (chicago, new york city, washington)
 
 
     # get user input for month (all, january, february, ... , june)
